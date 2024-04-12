0f002652399b0aa32c4c0f18f695335b99e7bc72
bimbayola <166798570+bimbayola@users.noreply.github.com>
2024-04-12 10:52:23 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static int i;
 
     public static void main(String[] args) {
-        System.out.println("Hello World and welcome!");
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Hello World!");
+        for (int i = 1; i <= 10; i++) {
             System.out.println("i = " + i);
         }
     }
