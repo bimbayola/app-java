079d03ca78af7f6eb860f58e44193f87460b1966
bimbayola <166798570+bimbayola@users.noreply.github.com>
2024-04-12 10:59:21 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static int i;
 
     public static void main(String[] args) {
-        String[] names = {"Piotr", "Anna", "Jan", "Leszek"};
+        String[] names = {"Wiosna", "Lato", "Jesień", "Zima"};
         for (int i = 0; i < names.length; i++) {
 System.out.println(i + 1 + ". " + names[i]);
         }
